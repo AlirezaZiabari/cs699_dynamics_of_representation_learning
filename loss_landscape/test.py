b76b5e608f155a593abb977ea11a6a9536b533b0
AliZ <alireza.ziabari@gmail.com>
2022-02-15 20:39:27 -0800

    new test method for both saved data and dataloader

--- loss_landscape/test.py
+++ loss_landscape/test.py
@@ -1,95 +1,64 @@
 import argparse
-import logging
-import os
-import pprint
-from re import L
-import time
-import json
-import numpy as np
-import dill
-import numpy.random
 import torch
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import Subset
-from torch.utils.tensorboard import SummaryWriter
 
-# from utils.evaluations import get_loss_value
-from utils.linear_algebra import FrequentDirectionAccountant
-from utils.nn_manipulation import count_params, flatten_grads
-from utils.reproducibility import set_seed
+from loss_landscape.train import get_dataloader
+from loss_landscape.utils.evaluations import get_loss_value_for_saved_data, get_loss_value
+
 from utils.resnet import get_resnet, set_resnet_weights
-import matplotlib.pyplot as plt
 
-from adversarial_attack import pgd_attack_l2, pgd_attack
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def load_adversarial_images(file_dir):
-    data = torch.load(file_dir)
-    return data["images"], data["labels"]
-
-
-def test_model():
-    root_dir = 'C:/Users/berkt/Desktop/cs699_dynamics_of_representation_learning/loss_landscape/results_final/'
-    model_name = 'resnet20'
-    remove_skip_connections = False
-    model_dir = root_dir + 'resnet20_skip_bn_bias'
-    ckpt_load = 200
-    device = 'cuda:0'
-    seed = 0
-    batch_size = 1
-
-    data_dir = root_dir + "resnet20_skip_bn_bias/"
-    save_name = 'pgd_image_'
-    train = True
-
-    if train:
-        number_of_files = 50
-        data_type_str = 'train'
-    else:
-        number_of_files = 10
-        data_type_str = 'test'
-
-    if not torch.cuda.is_available():
-        device = torch.device('cpu')
-    else:
-        device = torch.device('cuda:0')
-    model = get_resnet(model_name)(
-        num_classes=10, remove_skip_connections=remove_skip_connections
-    )
-
-    # load saved model
-    state_dict = torch.load(f"{model_dir}/ckpt/{ckpt_load}_model.pt", map_location=device)
-    model = set_resnet_weights(model, state_dict)
-    model.to(device)
-
+def test_model(model, device, use_saved_data=False, data_path=None, batch_size=1,
+               attack_type=None, eps=None, alpha=None, iterations=None):
     model.eval()
-
-    all_pred_labels = []
-    all_labels = []
-
-    for i in range(number_of_files):
-        images, labels = load_adversarial_images(data_dir + f'adv_{data_type_str}_data_pgd' + f"/pgd_image_{i}.pt")
-
-        images = images.to(device)
-        labels = labels.to(device)
-
-        pred_labels = model(images).argmax(axis=1).detach().to('cpu')
-
-        all_pred_labels.append(pred_labels)
-        all_labels.append(labels.detach())
-
-    all_pred_labels = torch.cat(all_pred_labels)
-    all_labels = torch.cat(all_labels)
-
-    acc = (all_labels.cpu() == all_pred_labels.cpu()).float().mean().data.numpy()
-
-    print(f"Test Accuracy on \n\
-            vanilla images: {acc}")
+    if use_saved_data:
+        losses, accuracies = get_loss_value_for_saved_data(model, data_path, device)
+    else:
+        _, test_loader = get_dataloader(batch_size=batch_size)
+        losses, accuracies = get_loss_value(model, test_loader, device,
+                                            attack_type=attack_type, eps=eps, alpha=alpha, iterations=iterations)
+    print(f"Test Accuracy: {accuracies}")
 
 
 if __name__ == '__main__':
-    test_model()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-D", "--debug", action='store_true')
+    parser.add_argument("--seed", required=False, type=int, default=0)
+    parser.add_argument(
+        "--device", required=False, default="cuda:0" if torch.cuda.is_available() else "cpu"
+    )
+
+    # model related arguments
+    parser.add_argument("--statefile", "-s", required=True, default=None)
+    parser.add_argument(
+        "--model", required=False, choices=["resnet20", "resnet32", "resnet44", "resnet56"], default="resnet20"
+    )
+    parser.add_argument("--remove_skip_connections", action="store_true", default=False)
+    parser.add_argument(
+        "--skip_bn_bias", action="store_true", default=True,
+        help="whether to skip considering bias and batch norm params or not, Li et al do not consider bias and batch norm params"
+    )
+
+    parser.add_argument("--use_saved_data", action="store_true", default=False)
+    parser.add_argument("--data_path", required=False, default=None)
+    parser.add_argument("--batch_size", required=False, type=int, default=1000)
+
+    parser.add_argument("--attack_type", required=False, type=str, default="vanilla")
+
+    parser.add_argument("--attack_eps", required=False, type=float, default=0.05)  # 2 for pgd_l2, 0.05 for pgd
+    parser.add_argument("--attack_alpha", required=False, type=float, default=0.05)  # 0.2 for pgd_l2, 0.05 for pgd
+    parser.add_argument("--attack_iters", required=False, type=int, default=20)
+
+    args = parser.parse_args()
+
+    model = get_resnet(args.model)(
+        num_classes=10, remove_skip_connections=args.remove_skip_connections
+    )
+    state_dict = torch.load(args.statefile, map_location=args.device)
+    model = set_resnet_weights(model, state_dict)
+
+    test_model(model. args.device, args.use_saved_data, args.data_path, args.batch_size,
+               args.attack_type, args.attack_eps, args.attack_alpha, args.attack_iters)
